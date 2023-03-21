f625bdc25194ac633667f7dee524ba03d0bdffa3
username <erosh.bc@gmail.com>
2023-03-21 05:43:24 -0500

    Initial Commit

---
+++ Proj4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Proj4
+//
+//  Created by Erosh Boralugodage on 3/21/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
