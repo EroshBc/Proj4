19d9233d07dc92da580b965a9f2a4b5a9357637b
username <erosh.bc@gmail.com>
2023-03-21 06:29:18 -0500

    create shared mem for clock

--- Proj4/oss.c
+++ Proj4/oss.c
@@ -6,10 +6,46 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define MAX_PROCESS 18;
+// Define the clock
+typedef struct{
+    unsigned int clock_sec;
+    unsigned int clock_nan;
+}Clock_t;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // generate a unique key for shared memory
+    key_t key = ftok("shared_mem_key", "R");\
+    
+    // create shared memory
+    int shmid = shmget(key, sizeof(Clock_t), IPC_CREAT|0666);
+    
+    if(shmid<0){
+        perror("shmget");
+        exit(1);
+    }
+    
+    //attached shared memory segment
+    Clock_t* shmem = (Clock_t*) shmat(shmid, NULL,0);
+    if(shmem ==(Clock_t*)-1){
+        perror("shmat");
+        exit(1);
+    }
+    
+    //initialized simulated system clock
+    shmem->clock_sec = 0;
+    shmem->clock_nan = 0;
+    
+    
+    
+    //Detach from the shared mem segment
+    shmdt(shmem);
+    
+    
     return 0;
 }
 
