07f8adc948f518b3eb2e830b9ae7f77f04a713a9
username <erosh.bc@gmail.com>
2023-03-21 06:45:44 -0500

    Create Process control board

--- Proj4/oss.c
+++ Proj4/oss.c
@@ -10,16 +10,30 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
-#define MAX_PROCESS 18;
+#define MAX_PROCESS 18
+
+// Define the Process control board
+
+typedef struct {
+    int pid;    // Process ID
+    int total_cpu_time;   // Total CPU time used
+    int total_time_in_system;   // Total time in the system
+    int local_pid;   // Local simulated PID
+} pcb_t;
+
 // Define the clock
 typedef struct{
     unsigned int clock_sec;
     unsigned int clock_nan;
+    pcb_t process_table[MAX_PROCESS];
 }Clock_t;
 
+
 int main(int argc, const char * argv[]) {
+    int i;
+    
     // generate a unique key for shared memory
-    key_t key = ftok("shared_mem_key", 15150);\
+    key_t key = ftok("shared_mem_key", 15150);
     
     // create shared memory
     int shmid = shmget(key, sizeof(Clock_t), IPC_CREAT|0666);
@@ -40,6 +54,13 @@ int main(int argc, const char * argv[]) {
     shmem->clock_sec = 0;
     shmem->clock_nan = 0;
     
+    //initialize the process control table
+    for(i=0; i<MAX_PROCESS; i++){
+        shmem->process_table[i].pid = -1; // Set PID to -1 to indicate an unused process control block
+        shmem->process_table[i].total_cpu_time = 0;
+        shmem->process_table[i].total_time_in_system = 0;
+        shmem->process_table[i].local_pid = i+1; // Set the local simulated PID
+    }
     
     
     //Detach from the shared mem segment
